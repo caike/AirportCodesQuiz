4f342b54bc072460798720e39acc3c0a9c9c0880
Carlos Souza <carloshrsouza@gmail.com>
2015-12-21 22:09:19 -0500

    Render answers

--- src/app.jsx
+++ src/app.jsx
@@ -14,10 +14,23 @@ class App extends React.Component {
 
 
 class Quiz extends React.Component {
+  constructor(props){
+    super(props);
+
+    // from http://www.2ality.com/2013/11/initializing-arrays.html
+    var arr = Array.apply(null, Array(this.props.data.length));
+    let displayQuestionFlag = arr.map(function (x, i) { return false });
+
+    displayQuestionFlag[0] = true;
+    console.log( displayQuestionFlag );
+
+    this.state = { displayQuestionFlag };
+  }
 
   render(){
     let airports = this.props.data.map( (airport, index) => {
-      return this._buildQuestionForAirport(airport);
+      return this._buildQuestionForAirport(airport,
+        this.state.displayQuestionFlag[index]);
     });
 
     return (<div>
@@ -29,18 +42,33 @@ class Quiz extends React.Component {
     );
   }
 
-  _buildQuestionForAirport(airport){
-    return(<Question {...airport} />);
+  _buildQuestionForAirport(airport, displayQuestion=false){
+    return(<Question {...airport} displayQuestion={displayQuestion} />);
   }
 }
 
 
 class Question extends React.Component {
+
   render(){
-    return(<div>
+    let display = this.props.displayQuestion ? "block" : "none";
+
+    return(<div className="card" style={{ display }}>
           <p>{this.props.name}</p>
-          <p>{this.props.code}</p>
+          <ul className="answer-list" style={{ listStyleType: "none" }}>
+            {this._buildAnswers()}
+          </ul>
          </div>);
+  }
+
+
+  _buildAnswers(){
+
+    let answerList = [0,1,2].map( (i) => {
+      return (<li><input type="radio" name="answer" /> {this.props.code}</li>);
+    }); // generate two random
+
+    return(<div>{answerList}</div>);
   }
 
 }
