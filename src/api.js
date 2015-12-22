217a0e73194ae3dd9411aa0c38fb2a1028a361b8
Carlos Souza <carloshrsouza@gmail.com>
2015-12-21 21:32:59 -0500

    Render questions

---
+++ src/api.js
@@ -0,0 +1,126 @@
+const API = {
+  "airports": [
+    {
+      "name": "Hartsfield Jackson Atlanta International United States",
+      "code": "ATL"
+    },
+    {
+      "name": "Beijing Capital International China",
+      "code": "PEK"
+    },
+    {
+      "name": "London Heathrow United Kingdom",
+      "code": "LHR"
+    },
+    {
+      "name": "Tokyo International Japan",
+      "code": "HND"
+    },
+    {
+      "name": "Chicago O’Hare International United States",
+      "code": "ORD"
+    },
+    {
+      "name": "Los Angeles International United States",
+      "code": "LAX"
+    },
+    {
+      "name": "Charles de Gaulle International France",
+      "code": "CDG"
+    },
+    {
+      "name": "Dallas Fort Worth International United States",
+      "code": "DFW"
+    },
+    {
+      "name": "Soekarno-Hatta International Indonesia",
+      "code": "CGK"
+    },
+    {
+      "name": "Dubai International United Arab Emirates",
+      "code": "DXB"
+    },
+    {
+      "name": "Frankfurt am Main International Germany",
+      "code": "FRA"
+    },
+    {
+      "name": "Hong Kong International Kai Tak Hong Kong",
+      "code": "HKG"
+    },
+    {
+      "name": "Denver International United States",
+      "code": "DEN"
+    },
+    {
+      "name": "Suvarnabhumi Thailand",
+      "code": "BKK"
+    },
+    {
+      "name": "Singapore Changi International Singapore",
+      "code": "SIN"
+    },
+    {
+      "name": "Amsterdam Schiphol Netherlands",
+      "code": "AMS"
+    },
+    {
+      "name": "John F Kennedy International United States",
+      "code": "JFK"
+    },
+    {
+      "name": "Guangzhou Baiyun International China",
+      "code": "CAN"
+    },
+    {
+      "name": "Madrid Barajas International Spain",
+      "code": "MAD"
+    },
+    {
+      "name": "Atatürk International Turkey",
+      "code": "IST"
+    },
+    {
+      "name": "Shanghai Pudong International China",
+      "code": "PVG"
+    },
+    {
+      "name": "San Francisco International United States",
+      "code": "SFO"
+    },
+    {
+      "name": "Charlotte Douglas International United States",
+      "code": "CLT"
+    },
+    {
+      "name": "McCarran International United States",
+      "code": "LAS"
+    },
+    {
+      "name": "Phoenix Sky Harbor International United States",
+      "code": "PHX"
+    },
+    {
+      "name": "George Bush Intercontinental Houston United States",
+      "code": "IAH"
+    },
+    {
+      "name": "Kuala Lumpur International Malaysia",
+      "code": "KUL"
+    },
+    {
+      "name": "Miami International United States",
+      "code": "MIA"
+    },
+    {
+      "name": "Incheon International South Korea",
+      "code": "ICN"
+    },
+    {
+      "name": "Munich International Germany",
+      "code": "MUC"
+    }
+  ]
+};
+
+export default API;
--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import API from "./api";
+
+class App extends React.Component {
+
+  render(){
+    return(<div className="quiz">
+             <Quiz data={this.props.list} />
+          </div>);
+  }
+
+}
+
 
 class Quiz extends React.Component {
+
   render(){
-    return (<div>I am the quiz</div>);
+    let airports = this.props.data.map( (airport, index) => {
+      return this._buildQuestionForAirport(airport);
+    });
+
+    return (<div>
+        <h2>Airport Code Quiz</h2>
+        <div className="questions">
+          {airports}
+        </div>
+      </div>
+    );
+  }
+
+  _buildQuestionForAirport(airport){
+    return(<Question {...airport} />);
   }
 }
 
-ReactDOM.render( <Quiz />, document.getElementById("container") );
+
+class Question extends React.Component {
+  render(){
+    return(<div>
+          <p>{this.props.name}</p>
+          <p>{this.props.code}</p>
+         </div>);
+  }
+
+}
+
+
+ReactDOM.render( <App list={API.airports}/>,
+                document.getElementById("container") );
 
