dc5f1697886e585b7dca8865c4b9a3360dbac16e
Carlos Souza <carloshrsouza@gmail.com>
2015-12-21 20:59:23 -0500

    Initial render

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require("gulp");
+var browserify = require("gulp-browserify");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+
+const ENTRY_POINT = "src/app.jsx";
+
+gulp.task("transpile", function() {
+    gulp.src(ENTRY_POINT)
+      .pipe(browserify({
+        debug: true,
+        transform: ["babelify", "reactify"]
+      }))
+      //.pipe(uglify())
+      .pipe(concat("bundle.js"))
+      .pipe(gulp.dest("public/"));
+});
+
+gulp.task("default",["transpile"]);
+
+gulp.task("watch", ["transpile"], function() {
+    gulp.watch("src/**/*.*", ["transpile"]);
+});
---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Quiz extends React.Component {
+  render(){
+    return (<div>I am the quiz</div>);
+  }
+}
+
+ReactDOM.render( <Quiz />, document.getElementById("container") );
+
