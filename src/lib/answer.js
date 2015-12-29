d42e681aeb8a0672964e84a74404df2c239da1d5
Carlos Souza <carloshrsouza@gmail.com>
2015-12-29 10:42:36 -0300

    Refactor for clarity

--- src/lib/answer.js
+++ src/lib/answer.js
@@ -5,9 +5,11 @@ const WRONG_ANSWER_BG = "btn-red-bg";
 
 function Answer(props){
 
+    let bgColorClass = props.bgColorClass || WRONG_ANSWER_BG;
+
     let buttonCxs = cx("pure-button", "pure-button-primary", {
       "is-answered": props.isAnswered,
-      [props.bgColorClass || WRONG_ANSWER_BG]: props.isAnswered
+      [bgColorClass]: props.isAnswered
     });
 
     return(<li key={props.key}><button
